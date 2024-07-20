a6eab8086956973152d584aa2dcf315d7fff1c31
William Wei <ziqiwei97@gmail.com>
2024-07-20 09:42:09 +0800

    add rabbitmq implementation of queue interface

--- threading_lib/task_queue.py
+++ threading_lib/task_queue.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import queue
+import pika
 
 class QueueInterface(ABC):
     @abstractmethod
@@ -12,6 +13,10 @@ class QueueInterface(ABC):
 
     @abstractmethod
     def task_done(self):
+        pass
+
+    @abstractmethod
+    def close(self):
         pass
 
 
@@ -27,6 +32,39 @@ class LocalQueue(QueueInterface):
     
     def task_done(self):
         self.q.task_done()
+
+    def close(self):
+        self.q.put(None)
  
 
+class RabbitMQQueue(QueueInterface):
+    def __init__(self, queue_name='task_queue'):
+        self.queue_name = queue_name
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue_name, durable=True)
+    
+    def put(self, item):
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.queue_name,
+            body=item,
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
 
+    def get(self, count=1):
+        messages = []
+        for _ in range(count):
+            method_frame, header_frame, body = self.channel.basic_get(queue=self.queue_name)
+            if method_frame:
+                self.channel.basic_ack(method_frame.delivery_tag)
+                messages.append(body)
+            else:
+                break
+        return messages
+    
+    def task_done(self):
+        pass
+
+    def close(self):
+        self.connection.close()
